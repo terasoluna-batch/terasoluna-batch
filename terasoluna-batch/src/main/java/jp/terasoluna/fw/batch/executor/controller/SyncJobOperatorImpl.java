af9cd1970d3a0456c11a7925c26e5fdcf59d5b83
mhasegawa <hasegawam@pm.nttdata.co.jp>
2015-12-25 01:11:31 +0900

    revice javadoc comment in SyncJobOperatorImpl #349 #280

--- terasoluna-batch/src/main/java/jp/terasoluna/fw/batch/executor/controller/SyncJobOperatorImpl.java
+++ terasoluna-batch/src/main/java/jp/terasoluna/fw/batch/executor/controller/SyncJobOperatorImpl.java
@@ -36,7 +36,7 @@ import org.springframework.util.Assert;
 import java.lang.reflect.InvocationTargetException;
 
 /**
- * 同期ジョブの起動クラス。<br>
+ * 同期型ジョブの実行を管理するためにフレームワークが提供するデフォルトの実装クラス。<br>
  * メインスレッドでビジネスロジックを実行する。<br>
  *
  * @since 3.6
