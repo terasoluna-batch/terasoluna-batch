0e72cbc7fa9d6d55ff8a7ebff5b89afe4c5d8057
yggd <hasegawam@pm.nttdata.co.jp>
2018-01-31 20:53:40 +0900

    append synchronized block in ApplicationContextResolverImpl.#470

---
+++ terasoluna-batch/src/test/java/jp/terasoluna/fw/batch/executor/dao/B000004Dao.java
@@ -0,0 +1,9 @@
+package jp.terasoluna.fw.batch.executor.dao;
+
+import jp.terasoluna.fw.batch.executor.HogeBean;
+
+import java.util.List;
+
+public interface B000004Dao {
+    List<HogeBean> findAll();
+}
